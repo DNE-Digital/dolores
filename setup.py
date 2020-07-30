fda1a486785137415e62c6c1cf973ad309814f90
pmohun <philmohun@gmail.com>
2020-07-30 02:31:51 -0400

    updated setup.py to include all json files, added package_data field to setup

--- setup.py
+++ setup.py
@@ -5,15 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dolores",
-    version="1.0.0",
+    version="1.0.3",
     author="Phil Mohun, Malcolm Navarro, DNE LLC",
     author_email="dolores@philmohun.com",
     description="Dolores is a Python library for developers using GPT-3.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dne-digital/calvin",
+    url="https://github.com/dne-digital/dolores",
     download_url="https://pypi.org/project/dolores/",
     packages=setuptools.find_packages(),
+    package_dir = {'dolores': 'dolores'},
+    package_data = {'dolores': ['dolores/*.json']},
+    #data_files = [('dolores', 'packages/prompts.json')],
     classifiers=[
         "Programming Language :: Python :: 3",
     	"License :: OSI Approved :: BSD License",
